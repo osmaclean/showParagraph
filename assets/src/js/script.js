ab8c21debf4b8520d7a03fc9793ec77ca11e11c9
Lucas Maclean <contatolucasmaclean@gmail.com>
2023-04-11 18:37:32 -0300

    Acréscimo de mais parágrafos

--- assets/src/js/script.js
+++ assets/src/js/script.js
@@ -9,27 +9,27 @@ paragraph.forEach((p, i) => {
         p.textContent = p.innerHTML.substring(0, maxLength) + '...';
 
         const btn = document.createElement('button');
-        btn.innerHTML = '<i class="fas fa-chevron-down"></i>'
+        btn.innerHTML = '<i class="fas fa-chevron-down"></i>';
         p.parentElement.classList.add('text-hidden');
 
         btn.addEventListener('click', function (e) {
-            toggleText(e, p, i)
+            toggleText(e, p, i);
         });
 
-        p.parentElement.appendChild(btn)
-    }
+        p.parentElement.appendChild(btn);
+    };
 });
 
 function toggleText(e, p, i) {
 
-    const card = e.currentTarget.parentElement
+    const card = e.currentTarget.parentElement;
 
-    card.classList.toggle('text-hidden')
+    card.classList.toggle('text-hidden');
     e.currentTarget.querySelector('i').classList.toggle('fa-chevron-down');
     e.currentTarget.querySelector('i').classList.toggle('fa-chevron-up');
 
     if (card.classList.contains('text-hidden')) {
-        card.querySelector('p').textContent = originTexts[i].substring(0, maxLength) + '...'
+        card.querySelector('p').textContent = originTexts[i].substring(0, maxLength) + '...';
     } else {
         card.querySelector('p').textContent = originTexts[i];
     };
